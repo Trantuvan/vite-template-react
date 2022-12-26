59f5d70c0533e6f6eb73584b0cc4c953dc53d66e
trantuvan <trantuvan.kan@gmail.com>
2022-12-26 16:58:32 +0700

    Scaffold React with vite
    + Setup prettier as editor's formatter
    + Setup eslint as editor's linter
    + Cleanup scaffold files

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React, { Component} from 'react';
+
+export default class App extends Component {
+  render(){
+    return <h1 className='text-3xl font-bold underline'>Hello World</h1>
+  }
+}
